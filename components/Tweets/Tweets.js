c4ebaa63c5bd9e782489dcb71b22bd2a1f86e0f4
Rahul A. Krishna <rkbalu524@gmail.com>
2018-01-04 21:58:11 +0530

    Update README

--- components/Tweets/Tweets.js
+++ components/Tweets/Tweets.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import {Text,View,ScrollView,StyleSheet} from 'react-native'
 import {connect} from 'react-redux'
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures'
+import FontAwesome from 'react-fontawesome'
 
 const styles=StyleSheet.create({
-    'tweet':{
+    tweet:{
         borderWidth:1,
         borderColor:'#a0a0a0',
         margin:2,
         padding:5
+    },
+    tweetText:{
+        fontSize:16
     }
 })
 
 class Tweets extends React.Component{
+    swipe=(direction,state)=>{
+        console.log(direction,state)
+    }
     render(){
         return(
             <ScrollView>
                 {this.props.tweets.map((tweet)=>(
-                    <View key={tweet.id} style={styles.tweet}>
-                        <Text>{tweet.text}</Text>
-                    </View>
+                    <GestureRecognizer
+                        key={tweet.id}
+                        onSwipe={(direction,state)=>{this.swipe(direction,state)}}
+                    >
+                        <View style={styles.tweet}>
+                            <Text style={styles.tweetText}>{tweet.text}</Text>
+                            <View>
+                                
+                            </View>
+                        </View>
+                    </GestureRecognizer>
                 ))}
             </ScrollView>
         )
